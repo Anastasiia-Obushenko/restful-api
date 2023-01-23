85e61e02876b828c909bdbb78d755d2fd0aadc32
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-01-23 17:44:57 -0500

    connect to DB

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // Routes
 app.get('/', (req, res) => {
     res.send("we are on home");
 });
-app.get('/posts', (req, res) => {
-    res.send("we are on posts");
-});
+
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, () =>
+    console.log("connected to DB")
+);
 
 app.listen(3000);
